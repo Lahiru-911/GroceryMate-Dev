8095a47b9710edf8cfe2daf13016af8ddcb4788b
Lahiru Kavinda <lahiruxk@gmail.com>
2025-03-13 23:09:42 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRouter from "./Routes/Router";
 import Footer from "./Components/Footer/Footer";
-import "./app.css";
+import "../src/style/app.css";
 import { NextUIProvider } from "@nextui-org/react";
 import NavBar from "./Components/Navbar/NavBar";
 
